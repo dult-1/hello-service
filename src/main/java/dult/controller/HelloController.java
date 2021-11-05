3fbf0b5af9254744d976dc463a92fbcccd2723b9
dult-1 <1769140517@qq.com>
2021-11-05 18:01:35 +0800

    测试Hystrix的合并器功能

--- src/main/java/dult/controller/HelloController.java
+++ src/main/java/dult/controller/HelloController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,6 +24,18 @@ public class HelloController {
         for(ServiceInstance instance:serviceInstanceList){
             System.out.println(" hello,host:"+instance.getHost()+" serverid:"+instance.getServiceId());
         }
-        return "hello eureka ";
+        return "hello eureka "+System.currentTimeMillis();
+    }
+
+    @RequestMapping("/findAll")
+    public List<String> findAll(@RequestParam("ids") List<String> ids){
+        System.out.println("===服务提供者收到findAll请求==="+ids);
+        List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("hello-service");
+        for(ServiceInstance instance:serviceInstanceList){
+            System.out.println(" hello,host:"+instance.getHost()+" serverid:"+instance.getServiceId());
+        }
+        List<String> resultList = new ArrayList<>();
+        resultList = ids;
+        return resultList;
     }
 }
